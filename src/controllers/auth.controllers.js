d46ea846b593633b242656f46e94c435def50327
Bishal Jana <bishaljana2026@gmail.com>
2024-03-20 08:37:41 +0530

    login router check

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,5 +1,7 @@
 import { User } from "../models/user.models.js";
 import bcrypt from "bcrypt";
+
+import jwt from "jsonwebtoken";
 export const Register = async (req, res) => {
   try {
     const { name, username, email, password } = req.body;
@@ -32,5 +34,58 @@ export const Register = async (req, res) => {
     });
   } catch (error) {
     console.log(error);
+  }
+};
+
+//Login
+
+export const Login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(401).json({
+        message: "email or password are not present",
+        success: false,
+      });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({
+        message: "user does not exist",
+        success: false,
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(401).json({
+        message: "passwords do not match",
+        success: false,
+      });
+    }
+
+    const tokenData = {
+      userId: user._id,
+    };
+    const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET, {
+      expiresIn: "1d",
+    });
+    return res
+      .status(201)
+      .cookie("token", token, { expiresIn: "1d", httpOnly: true })
+      .json({
+        message: `welcome back ${user.name}`,
+        success: true,
+      });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Internal server error",
+      success: false,
+    });
   }
 };
--- src/routers/user.routers.js
+++ src/routers/user.routers.js
@@ -1,7 +1,8 @@
 import Router from "express";
-import { Register } from "../controllers/auth.controllers.js";
+import { Login, Register } from "../controllers/auth.controllers.js";
 
 const router = Router();
 router.route("/register").post(Register);
+router.route("/login").post(Login);
 
 export default router;
