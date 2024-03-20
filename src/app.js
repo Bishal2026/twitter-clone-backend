1303f26196fbb5292c08df760dfc4452b65912e1
Bishal Jana <bishaljana2026@gmail.com>
2024-03-20 23:43:57 +0530

    cors add

--- src/app.js
+++ src/app.js
@@ -1,15 +1,8 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-// import cors from "cors";
+import cors from "cors";
 
 const app = express();
-
-// app.use(
-//   cors({
-//     origin: process.env.CORS_ORIGIN,
-//     credentials: true,
-//   })
-// );
 
 app.use(
   express.json({
@@ -18,6 +11,13 @@ app.use(
 );
 
 app.use(express.urlencoded({ extended: true, limit: "20kb" }));
+
+const coreOrigin = {
+  origin: "http://localhost:3000",
+  credentials: true,
+};
+
+app.use(cors(coreOrigin));
 
 app.use(express.static("public"));
 
